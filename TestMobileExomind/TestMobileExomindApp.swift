45c40828150bdc4ec97432401615307f6f2eef93
Pierre Lemère <pierre.lemere@gmail.com>
2023-02-09 10:58:34 +0100

    Initial Commit

---
+++ TestMobileExomind/TestMobileExomindApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMobileExomindApp.swift
+//  TestMobileExomind
+//
+//  Created by Pierre on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestMobileExomindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
